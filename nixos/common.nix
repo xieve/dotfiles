a4037892dd83992525f26abc60c9dddef8b3dcbf
xieve <2876434-xieve@users.noreply.gitlab.com>
2024-03-01 22:29:00 +0100

    refactor(common.nix): better structure

--- nixos/common.nix
+++ nixos/common.nix
@@ -1,78 +1,85 @@
 { config, lib, pkgs, ... }:
 
 {
-	config = {
-		nix = {
-			package = pkgs.nixFlakes;
-			extraOptions = ''
-				experimental-features = nix-command flakes
-			'';
-		};
+	# Enable flakes
+	nix = {
+		package = pkgs.nixFlakes;
+		extraOptions = ''
+			experimental-features = nix-command flakes
+		'';
+	};
 
-		# Bootloader
-		boot.loader.systemd-boot.enable = true;
-		boot.loader.efi.canTouchEfiVariables = true;
 
-		# TZ & Locale
-		time.timeZone = "Europe/Berlin";
+	# Bootloader
+	boot.loader.systemd-boot.enable = true;
+	boot.loader.efi.canTouchEfiVariables = true;
 
-		i18n.defaultLocale = "en_US.UTF-8";
 
-		i18n.extraLocaleSettings = {
-			LC_ADDRESS = "de_DE.UTF-8";
-			LC_IDENTIFICATION = "de_DE.UTF-8";
-			LC_MEASUREMENT = "de_DE.UTF-8";
-			LC_MONETARY = "de_DE.UTF-8";
-			LC_NAME = "de_DE.UTF-8";
-			LC_NUMERIC = "de_DE.UTF-8";
-			LC_PAPER = "de_DE.UTF-8";
-			LC_TELEPHONE = "de_DE.UTF-8";
-			LC_TIME = "de_DE.UTF-8";
-		};
+	# TZ & Locale
+	time.timeZone = "Europe/Berlin";
 
-		services.xserver.xkb.layout = "us";
+	i18n.defaultLocale = "en_US.UTF-8";
 
-		# Default user
-		users.users.xieve = {
-			isNormalUser = true;
-			description = "xieve";
-			extraGroups = [ "wheel" ];
-			# User pkgs
-			packages = with pkgs; [
-				dnsutils
-				mosh
-			];
-		};
+	i18n.extraLocaleSettings = {
+		LC_ADDRESS = "de_DE.UTF-8";
+		LC_IDENTIFICATION = "de_DE.UTF-8";
+		LC_MEASUREMENT = "de_DE.UTF-8";
+		LC_MONETARY = "de_DE.UTF-8";
+		LC_NAME = "de_DE.UTF-8";
+		LC_NUMERIC = "de_DE.UTF-8";
+		LC_PAPER = "de_DE.UTF-8";
+		LC_TELEPHONE = "de_DE.UTF-8";
+		LC_TIME = "de_DE.UTF-8";
+	};
 
-		# zsh & direnv
-		environment.shells = with pkgs; [
-			bashInteractive
-			zsh
+	services.xserver.xkb.layout = "us";
+
+
+	# Default user
+	users.users.xieve = {
+		isNormalUser = true;
+		description = "xieve";
+		extraGroups = [ "wheel" ];
+		# User pkgs
+		packages = with pkgs; [
+			dnsutils
+			git-crypt
+			mosh
 		];
-		programs.zsh.enable = true;
-		programs.direnv.enable = true;
-		users.defaultUserShell = pkgs.zsh;
-
-		# allow unfree pkgs
-		nixpkgs.config.allowUnfree = true;
-
-		# system pkgs
-		environment.systemPackages = with pkgs; [
-			git
-			htop
-			killall
-			python3
-			tmux
-			unzip
-			zsh
-		];
-
-		programs.neovim = {
-			enable = true;
-			defaultEditor = true;
-		};
 	};
 
 	# If NM is enabled, allow default user to manage it
-	config.users.groups.networkmanager.members = lib.mkIf config.networking.networkmanager.enable [ "xieve" ];
+	users.groups.networkmanager.members = lib.mkIf config.networking.networkmanager.enable [ "xieve" ];
+
+
+	# zsh & direnv
+	environment.shells = with pkgs; [
+		bashInteractive
+		zsh
+	];
+	programs.zsh.enable = true;
+	programs.direnv.enable = true;
+	users.defaultUserShell = pkgs.zsh;
+
+
+	# allow unfree pkgs
+	nixpkgs.config.allowUnfree = true;
+
+
+	# system pkgs
+	environment.systemPackages = with pkgs; [
+		git
+		htop
+		killall
+		python3
+		tmux
+		unzip
+		zsh
+	];
+
+
+	programs.neovim = {
+		enable = true;
+		defaultEditor = true;
+	};
 }
