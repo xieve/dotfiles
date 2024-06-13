59ba353e83b340747c49bb87366e5f4da60f949e
xieve <2876434-xieve@users.noreply.gitlab.com>
2024-06-13 12:18:02 +0200

    feat(thegreatbelow): smbd

--- nixos/thegreatbelow/configuration.nix
+++ nixos/thegreatbelow/configuration.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, nzbr, ... }:
+{ config, lib, pkgs, nzbr, ... }:
 
 {
 	imports = [
@@ -43,6 +43,7 @@
 
 
 	# temp fix for perms as long as this is a VM
+	# user is called "nobody" on unraid
 	users.users.unraidnobody = {
 		uid = 99;
 		group = "users";
@@ -54,7 +55,67 @@
 		qemuGuest.enable = true;
 		openssh.enable = true;
 		searx.enable = true;
+		samba = {
+			enable = true;
+			openFirewall = true;
+			shares = {
+				public = {
+					path = "/mnt/user/public";
+					writeable = "yes";
+					public = "yes";
+					#"guest ok" = "yes";
+				};
+			};
+			extraConfig = ''
+				workgroup = WORKGROUP
+				#server string = The Great Below
+				#netbios name = ${lib.toUpper config.networking.hostName}
+
+				log level = 4
+
+				use sendfile = yes
+				deadtime = 30
+
+				# smbpasswd will set the unix password as well
+				unix password sync = yes
+
+				hosts allow = 192.168.0. 127.0.0.1 localhost
+				hosts deny = ALL
+				server min protocol = SMB3_11
+				client ipc min protocol = SMB3_11
+				client signing = mandatory
+				server signing = mandatory
+				client ipc signing = mandatory
+				client NTLMv2 auth = yes
+				smb encrypt = required
+				restrict anonymous = 2
+				null passwords = No
+				raw NTLMv2 auth = no
+				map to guest = never
+				access based share enum = yes
+				guest account = nobody
+
+				browseable = yes
+				force create mode = 0666
+				force directory mode = 0777
+				force user = unraidnobody
+				force group = users
+				create mask = 0666
+				directory mask = 0777
+			'';
+		};
+		samba-wsdd = {
+			enable = true;
+			openFirewall = true;
+			extraOptions = [ "--verbose" ];
+		};
 	};
+	# allow multicast for wsdd
+	#system.activationScripts = { samba-wsdd.text = "${pkgs.ipset}/bin/ipset create samba-wsdd hash:ip,port timeout 3 -exist"; };
+	networking.firewall.extraCommands = ''
+		iptables -A nixos-fw -p udp -m pkttype --pkt-type multicast -m udp --dport 3702 -d 239.255.255.250/32 -j nixos-fw-accept
+		ip6tables -A nixos-fw -p udp -m pkttype --pkt-type multicast -m udp --dport 3702 -d ff02::c/128 -j nixos-fw-accept
+	'';
 	programs.mosh.enable = true;
 
 
