9d3a9d5358710816f23325ea01880789aa365fa1
xieve <2876434-xieve@users.noreply.gitlab.com>
2024-06-20 00:15:46 +0200

    refactor: move gnome config to separate file

--- nixos/despacito3/configuration.nix
+++ nixos/despacito3/configuration.nix
@@ -1,37 +1,28 @@
 { config, pkgs, ... }:
 
-{
+let
+	home = config.users.users.xieve.home;
+in {
 	imports = [
 		./hardware.nix
 		../common.nix
+		../gnome.nix
 	];
 
 
 	networking = {
 		hostName = "despacito3";
-		networkmanager.enable = true;
 	};
 
 
-	hardware.bluetooth = {
-		enable = true;
-		powerOnBoot = false;
-	};
+	hardware = {
+		bluetooth = {
+			enable = true;
+			powerOnBoot = false;
+		};
 
-
-	# Hardware decoding
-	nixpkgs.config.packageOverrides = pkgs: {
-		intel-vaapi-driver = pkgs.intel-vaapi-driver.override { enableHybridCodec = true; };
-	};
-
-	hardware.opengl = {
-		enable = true;
-		extraPackages = with pkgs; [
-			intel-media-driver # LIBVA_DRIVER_NAME=iHD
-			#intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
-			vaapiVdpau
-			libvdpau-va-gl
-		];
+		# Enable screen rotation
+		sensor.iio.enable = true;
 	};
 
 	environment.sessionVariables = { LIBVA_DRIVER_NAME = "iHD"; }; # Force intel-media-driver
@@ -42,27 +33,12 @@
 
 
 	services = {
-		# Desktop stuff
-		xserver = {
-			enable = true;
-			desktopManager.plasma5.enable = true;
-		};
-
-		displayManager = {
-			sddm.enable = true;
-			defaultSession = "plasmawayland";
-		};
-
-		# Enable CUPS
-		printing.enable = true;
-
-
 		syncthing = {
 			enable = true;
 			user = "xieve";
-			dataDir = "/home/xieve";
+			dataDir = home;
 			openDefaultPorts = true;
-			configDir = "/home/xieve/.config/syncthing";
+			configDir = "${home}/.config/syncthing";
 		};
 
 
@@ -76,46 +52,19 @@
 	};
 
 
-	# Enable sound with pipewire.
-	sound.enable = true;
-	hardware.pulseaudio.enable = false;
-	security.rtkit.enable = true;
-	services.pipewire = {
-		enable = true;
-		alsa.enable = true;
-		alsa.support32Bit = true;
-		pulse.enable = true;
-		# If you want to use JACK applications, uncomment this
-		#jack.enable = true;
-
-		# use the example session manager (no others are packaged yet so this is enabled by default,
-		# no need to redefine it in your config for now)
-		#media-session.enable = true;
-	};
-
-
 	# User pkgs
-	users.users.xieve = {
-		packages = with pkgs; [
-			(nerdfonts.override { fonts = [ "FiraCode" "NerdFontsSymbolsOnly" ]; })
-			bespokesynth
-			dolphin-emu
-			fira-code
-			firefox
-			flavours
-			kate
-			keepassxc
-			maliit-keyboard
-			moonlight-qt
-			obsidian
-			syncthingtray
-			vscodium
-			wezterm
-			wineWowPackages.stagingFull
-			yabridge
-			yabridgectl
-		];
-	};
+	users.users.xieve.packages = with pkgs; [
+		bespokesynth
+		dolphin-emu
+		flavours
+		jetbrains.clion
+		moonlight-qt
+		syncthingtray
+		wezterm
+		wineWowPackages.stagingFull
+		yabridge
+		yabridgectl
+	];
 
 
 	# Dolphin emu udev rules (allow direct bluetooth access)
