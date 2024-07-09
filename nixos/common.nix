d703992523a526484a2c9fe55fdb4e33ffbeeda4
xieve <2876434-xieve@users.noreply.gitlab.com>
2024-07-09 23:43:22 +0200

    feat(nixos): declare location of flake

--- nixos/common.nix
+++ nixos/common.nix
@@ -102,6 +102,11 @@ in
     defaultEditor = true;
   };
 
+  # This will break nixos-rebuild on system which don't have my dotfiles cloned
+  # to this location, but you can always resort to specifying the config
+  # location manually.
+	environment.etc."nixos/flake.nix".source = "/home/xieve/.dotfiles/nixos/flake.nix";
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
