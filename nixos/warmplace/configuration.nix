1cf01275ca623fe423eef46d1a5852eb22ce341c
xieve <2876434-xieve@users.noreply.gitlab.com>
2024-07-16 23:06:29 +0200

    feat(warmplace): enable hass

--- nixos/warmplace/configuration.nix
+++ nixos/warmplace/configuration.nix
@@ -32,5 +32,38 @@
   services = {
     openssh.enable = true;
     tailscale.enable = true;
+
+    home-assistant = {
+      enable = true;
+      extraComponents = [
+        # Components required to complete the onboarding
+        "esphome"
+        "met"
+        "radio_browser"
+      ];
+      config = {
+        # Includes dependencies for a basic setup
+        # https://www.home-assistant.io/integrations/default_config/
+        default_config = { };
+        http = {
+          trusted_proxies = [ "::1" ];
+          use_x_forwarded_for = true;
+        };
+      };
+    };
+
+    nginx = {
+      enable = true;
+      recommendedProxySettings = true;
+      virtualHosts."warmplace" = {
+        extraConfig = ''
+          proxy_buffering off;
+        '';
+        locations."/" = {
+          proxyPass = "http://[::1]:8123";
+          proxyWebsockets = true;
+        };
+      };
+    };
   };
 }
