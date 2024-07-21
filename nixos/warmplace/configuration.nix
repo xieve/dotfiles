d6f68d973fe7fbb7ca3d15121e00a7a2718c1212
xieve <2876434-xieve@users.noreply.gitlab.com>
2024-07-21 22:51:33 +0200

    feat(warmplace): PV integration into hass via MQTT

--- nixos/warmplace/configuration.nix
+++ nixos/warmplace/configuration.nix
@@ -5,7 +5,9 @@
   modulesPath,
   ...
 }:
-
+let
+  secrets = lib.importTOML ./secrets.toml;
+in
 {
   imports = [
     ../common.nix
@@ -36,7 +38,23 @@
   services = {
     openssh.enable = true;
     tailscale.enable = true;
-
+    mosquitto = {
+      enable = true;
+      listeners = [
+        {
+          users = {
+            growatt = {
+              acl = [ "readwrite energy/solar/#" ];
+              password = secrets.mosquitto.growatt;
+            };
+            hass = {
+              acl = [ "readwrite #" ];
+              password = secrets.mosquitto.hass;
+            };
+          };
+        }
+      ];
+    };
     home-assistant = {
       enable = true;
       extraComponents = [
@@ -49,9 +67,80 @@
         # Includes dependencies for a basic setup
         # https://www.home-assistant.io/integrations/default_config/
         default_config = { };
+
         http = {
           trusted_proxies = [ "::1" ];
           use_x_forwarded_for = true;
+        };
+
+        mqtt = {
+          sensor =
+            lib.concatMap
+              (
+                { name, value }:
+                map (
+                  jsonKey:
+                  let
+                    device_class = name;
+                    serial_number = "CZM0DCT004";
+                    model = "Growatt MOD 10KTL3-XH";
+                  in
+                  (
+                    {
+                      inherit device_class;
+                      name = jsonKey;
+                      state_topic = "energy/solar";
+                      value_template = "{{ value_json.${jsonKey} }}";
+                      availability_topic = "energy/solar";
+                      availability_template = "{{ value_json.InverterStatus }}";
+                      payload_available = "1";
+                      device = {
+                        inherit model serial_number;
+                        name = "Wechselrichter";
+                        configuration_url = "http://growatt.fritz.box";
+                        via_device = "OpenInverterGateway on Growatt ShineWiFi-X";
+                        identifiers = [
+                          model
+                          serial_number
+                        ];
+                      };
+                      state_class = "measurement";
+                      unique_id = "${serial_number}_${jsonKey}";
+                    }
+                    // lib.optionalAttrs (device_class == "power") { unit_of_measurement = "W"; }
+                    // lib.optionalAttrs (device_class == "temperature") { unit_of_measurement = "°C"; }
+                    // lib.optionalAttrs (device_class == "battery") { unit_of_measurement = "%"; }
+                    // lib.optionalAttrs (device_class == "energy") {
+                      unit_of_measurement = "kWh";
+                      state_class = "total_increasing";
+                    }
+                  )
+                ) value
+              )
+              (
+                lib.attrsToList {
+                  energy = [
+                    "TotalEnergyOfUserLoad"
+                    "TotalEnergyToGrid"
+                    "TotalEnergyToUser"
+                    "TotalGenerateEnergy"
+                  ];
+                  temperature = [
+                    "BDCTemperatureA"
+                    "InverterTemperature"
+                  ];
+                  battery = [ "BDCStateOfCharge" ];
+                  power = [
+                    "BDCChargePower"
+                    "BDCDischargePower"
+                    "OutputPower"
+                    "PVTotalPower"
+                    "TotalForwardPower"
+                    "TotalLoadPower"
+                    "TotalReversePower"
+                  ];
+                }
+              );
         };
       };
     };
@@ -70,4 +159,9 @@
       };
     };
   };
+
+  networking.firewall.allowedTCPPorts = [
+    1883 # MQTT
+    80
+  ];
 }
