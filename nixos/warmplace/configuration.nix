40f3f44fc88a92777dfe2a64b72e8b9bfe2d1156
xieve <2876434-xieve@users.noreply.gitlab.com>
2024-09-02 10:04:04 +0200

    feat(warmplace): add evcc

--- nixos/warmplace/configuration.nix
+++ nixos/warmplace/configuration.nix
@@ -36,186 +36,263 @@ in
     ZSH_TMUX_AUTOSTART = "true";
   };
 
+  # allow access to acme folder for both nginx and mosquitto so they can share a tls cert
+  security.acme.certs.${config.networking.hostName}.group = "acme";
+  users.users.mosquitto.extraGroups = [ "acme" ];
+  users.users.nginx.extraGroups = [ "acme" ];
+
   services = {
     openssh.enable = true;
     tailscale.enable = true;
+    avahi.enable = true; # mdns, needed by esphome
+
     mosquitto = {
       enable = true;
       listeners = [
         {
-          users = {
-            growatt = {
-              acl = [ "readwrite energy/solar/#" ];
-              hashedPassword = secrets.mosquitto.growatt;
+          settings =
+            let
+              # re-use nginx's keys
+              acmeDir = config.security.acme.certs.${config.networking.hostName}.directory;
+            in
+            {
+              # go-e Charger currently doesn't seem to work with MQTTS
+              #certfile = "${acmeDir}/cert.pem";
+              #keyfile = "${acmeDir}/key.pem";
             };
+          users = {
             hass = {
+              # HA connects via localhost which means it doesn't need to authenticate
+              # Keeping this as a debug credential
               acl = [ "readwrite #" ];
               hashedPassword = secrets.mosquitto.hass;
             };
             goe = {
               acl = [
                 "readwrite homeassistant/#"
-                "readwrite go-eCharger/#"
+                "readwrite energy/go-eCharger/#"
               ];
               hashedPassword = secrets.mosquitto.goe;
             };
           };
         }
+        {
+          # localhost access without auth
+          address = "::1";
+          port = 11883;
+          acl = [ "pattern readwrite #" ];
+          omitPasswordAuth = true;
+          settings.allow_anonymous = true;
+        }
       ];
     };
+
+    evcc = {
+      enable = true;
+      settings = {
+        network = {
+          schema = "https";
+          host = config.networking.hostName;
+          port = 7070;
+        };
+        mqtt = {
+          broker = "localhost:11883";
+          topic = "energy/evcc";
+        };
+        meters =
+          let
+            goeMqtt = jsonKey: {
+              type = "custom";
+              power = {
+                source = "mqtt";
+                # reusing the data we're giving the charger since it's almost in the correct format
+                # (i.e. one number that can go pos/neg)
+                topic = "energy/go-eCharger/ids/set";
+                jq = ".${jsonKey}";
+              };
+            };
+          in
+          [
+            ({ name = "grid"; } // goeMqtt "pGrid")
+            ({ name = "pv"; } // goeMqtt "pPv")
+            ({
+              name = "battery";
+              # state of charge is published by HA via statestream integration,
+              # HA is connected to growatt (esphome device) via HA api
+              soc = {
+                source = "mqtt";
+                topic = "homeassistant/sensor/growatt_battery_charge/state";
+              };
+            } // goeMqtt "pAkku")
+          ];
+        chargers = [
+          {
+            # this opens a websocket on port 8887, which we reverse proxy with nginx for TLS
+            name = "ocpp";
+            type = "template";
+            template = "ocpp";
+          }
+        ];
+        vehicles = [
+          {
+            # Connected via uconnect cloud service for vehicle state-of-charge data
+            name = "fiat500e";
+            title = "Fiat 500e";
+            type = "template";
+            template = "fiat";
+            user = secrets.fiat.user;
+            password = secrets.fiat.password;
+            capacity = 39;
+          }
+        ];
+        tariffs.co2 = {
+          type = "grünstromindex";
+          zip = secrets.zip;
+        };
+        site = {
+          title = "Zuhause";
+          meters = {
+            grid = "grid";
+            pv = [ "pv" ];
+            battery = [ "battery" ];
+          };
+        };
+        loadpoints = [
+          {
+            title = "Werkstatt";
+            charger = "ocpp";
+            # let's see if i need to define this in addition to OCPP TODO
+            #meter = "charge";
+          }
+        ];
+      };
+    };
+
     home-assistant = {
       enable = true;
       extraComponents = [
-        # Components required to complete the onboarding
         "esphome"
         "met"
+        "mqtt"
         "radio_browser"
         "forecast_solar"
       ];
-      customComponents = [ self-pkgs.homeassistant-goecharger-mqtt ];
-      config =
-        let
-          growattSensors = {
-            energy = [
-              "ChargeEnergyTotal"
-              "DischargeEnergyTotal"
-              "TotalEnergyOfUserLoad"
-              "TotalEnergyToGrid"
-              "TotalEnergyToUser" # Energy drawn from grid
-              "TotalGenerateEnergy"
-            ];
-            temperature = [
-              "BDCTemperatureA"
-              "InverterTemperature"
-            ];
-            battery = [ "BDCStateOfCharge" ];
-            power = [
-              "BDCChargePower"
-              "BDCDischargePower"
-              "OutputPower"
-              "PVTotalPower"
-              "ReactivePower"
-              "TotalForwardPower"
-              "TotalLoadPower"
-              "TotalReversePower"
-            ];
-          };
-          serial_number = "CZM0DCT004";
-          model = "MOD 10KTL3-XH";
-        in
-        {
-          # Includes dependencies for a basic setup
-          # https://www.home-assistant.io/integrations/default_config/
-          default_config = { };
+      config = {
+        # Includes dependencies for a basic setup
+        # https://www.home-assistant.io/integrations/default_config/
+        default_config = { };
 
-          http = {
-            trusted_proxies = [ "::1" ];
-            use_x_forwarded_for = true;
-          };
-
-          mqtt = {
-            sensor = lib.concatMap (
-              { name, value }:
-              map (
-                jsonKey:
-                let
-                  device_class = name;
-                in
-                (
-                  {
-                    inherit device_class;
-                    name = jsonKey;
-                    state_topic = "energy/solar";
-                    value_template = "{{ value_json.${jsonKey} }}";
-                    availability_topic = "energy/solar";
-                    availability_template = "{{ value_json.InverterStatus }}";
-                    payload_available = "1";
-                    expire_after = 300;
-                    device = {
-                      manufacturer = "Growatt";
-                      inherit model serial_number;
-                      name = "Wechselrichter";
-                      configuration_url = "http://growatt.fritz.box";
-                      via_device = "OpenInverterGateway on Growatt ShineWiFi-X";
-                      identifiers = [
-                        model
-                        serial_number
-                      ];
-                    };
-                    state_class = "measurement";
-                    unique_id = "${serial_number}_${jsonKey}";
-                  }
-                  // lib.optionalAttrs (device_class == "power") { unit_of_measurement = "W"; }
-                  // lib.optionalAttrs (device_class == "temperature") { unit_of_measurement = "°C"; }
-                  // lib.optionalAttrs (device_class == "battery") { unit_of_measurement = "%"; }
-                  // lib.optionalAttrs (device_class == "energy") {
-                    unit_of_measurement = "kWh";
-                    state_class = "total_increasing";
-                  }
-                )
-              ) value
-            ) (lib.attrsToList growattSensors);
-          };
-
-          sensor = map (powerSensor: {
-            name = lib.replaceStrings [ "Power" ] [ "Energy" ] powerSensor;
-            platform = "integration";
-            source = "sensor.wechselrichter_${lib.toLower powerSensor}";
-            unit_prefix = "k";
-          }) growattSensors.power;
-
-          automation = [
-            {
-              alias = "go-e Surplus Charging";
-              description = "Feed PV data into go-e Charger for surplus charging";
-              trigger = {
-                platform = "time_pattern";
-                seconds = "/5";
-              };
-              condition.not = {
-                condition = "state";
-                entity_id = "sensor.wechselrichter_pvtotalpower";
-                state = "unavailable";
-              };
-              action = {
-                service = "mqtt.publish";
-                data = {
-                  # go-e Charger topic is `go-eCharger/<Serial number>` by default
-                  topic = "go-eCharger/235184/ids/set";
-                  payload_template = ''
-                    {"pGrid":{% if float(states("sensor.wechselrichter_totalforwardpower"))>=0 -%}
-                      {{ states("sensor.wechselrichter_totalforwardpower") }}
-                    {%- else -%}
-                      -{{ states("sensor.wechselrichter_totalreversepower") }}
-                    {%- endif %},
-                    "pAkku":{% if float(states("sensor.wechselrichter_bdcdischargepower"))>=0 -%}
-                      {{ states("sensor.wechselrichter_bdcdischargepower") }}
-                    {%- else -%}
-                      -{{ states("sensor.wechselrichter_bdcchargepower") }}
-                    {%- endif %},
-                    "pPv":{{ states("sensor.wechselrichter_pvtotalpower") }}}
-                  '';
-                };
-              };
-            }
-          ];
+        http = {
+          trusted_proxies = [ "::1" ];
+          use_x_forwarded_for = true;
         };
+
+        # growatt is auto-discovered (HA api), we do our own energy calculations
+        # based on polled power data (integral)
+        sensor =
+          map
+            (str: {
+              name = lib.replaceStrings [ "Power" ] [ "Energy" ] str;
+              platform = "integration";
+              source = "sensor.growatt_${lib.toLower (lib.replaceStrings [ " " ] [ "_" ] str)}";
+              unit_prefix = "k"; # kWh
+            })
+            [
+              "Power to Grid"
+              "Power from Grid"
+              "Power to Battery"
+              "Power from Battery"
+              "PV Total Power"
+            ];
+
+        # Automatically publish all state changes we receive from growatt (connected via native HA api)
+        # evcc reads state-of-charge from here
+        mqtt_statestream = {
+          base_topic = "homeassistant";
+          include = {
+            entity_globs = [
+              "sensor.growatt_*"
+            ];
+          };
+        };
+
+        automation = [
+          {
+            alias = "go-e Surplus Charging";
+            description = "Feed PV data into go-e Charger for surplus charging";
+            trigger = {
+              platform = "time_pattern";
+              seconds = "/5";
+            };
+            condition = {
+              condition = "state";
+              entity_id = "sensor.growatt_status";
+              state = "Normal";
+            };
+            action = {
+              service = "mqtt.publish";
+              data = {
+                # go-e Charger topic is `go-eCharger/<Serial number>` by default
+                # but can be changed in the app
+                topic = "energy/go-eCharger/ids/set";
+                payload_template = ''
+                  {"pGrid":{{
+                    float(states("sensor.growatt_power_from_grid"))
+                      - float(states("sensor.growatt_power_to_grid"))
+                  }},
+                  "pAkku":{{
+                    float(states("sensor.growatt_power_from_battery"))
+                      - float(states("sensor.growatt_power_to_battery"))
+                  }},
+                  "pPv":{{ states("sensor.growatt_pv_total_power") }}}
+                '';
+              };
+            };
+          }
+        ];
+      };
     };
 
     nginx = {
       enable = true;
       recommendedProxySettings = true;
       recommendedTlsSettings = true;
-      virtualHosts."warmplace" = {
+      virtualHosts.${config.networking.hostName} = {
         # We're abusing the fallback self-signed cert here
         enableACME = true;
         forceSSL = true;
         extraConfig = ''
           proxy_buffering off;
         '';
-        locations."/" = {
-          proxyPass = "http://[::1]:8123";
-          proxyWebsockets = true;
+        locations = {
+          "/" = {
+            proxyWebsockets = true;
+            extraConfig = ''
+              # https://serverfault.com/questions/586586/nginx-redirect-via-proxy-rewrite-and-preserve-url
+              # evcc doesn't support running behind a reverse proxy but we're obviously gonna do it anyway
+              # by using the Referer header we can infer the "proxied" URL from the wrong one
+              add_header Vary Referer;
+
+              # This dumb workaround is in place because NixOS forces me to
+              # pass a piece of shit "validation" with this config.
+              # https://github.com/NixOS/nixpkgs/issues/128506
+              # fuck you
+              set $fuck_you_gixy http_referer;
+              if ($fuck_you_gixy ~ ^https://[^/]*/(evcc|esphome)) {
+                return 302 /$1/$request_uri;
+              }
+              proxy_pass http://[::1]:8123;
+            '';
+          };
+          "/evcc/" = {
+            proxyWebsockets = true;
+            proxyPass = "http://[::1]:7070/";
+          };
+          # Open Charging Point Protocol, handled by evcc
+          "/ocpp/" = {
+            proxyWebsockets = true;
+            proxyPass = "http://[::1]:8887/";
+          };
         };
       };
     };
@@ -231,7 +308,7 @@ in
     acceptTerms = true;
   };
   # Ignore error produced due to config above
-  systemd.services.acme-warmplace.serviceConfig.SuccessExitStatus = [ 10 ];
+  systemd.services."acme-${config.networking.hostName}".serviceConfig.SuccessExitStatus = [ 10 ];
 
   networking.firewall.allowedTCPPorts = [
     1883 # MQTT
