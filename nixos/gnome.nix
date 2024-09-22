abe85fc76abf718fceccfb0799894d3d15e9b28d
xieve <2876434-xieve@users.noreply.gitlab.com>
2024-09-22 19:26:36 +0200

    chore(gnome): match upstream changes

--- nixos/gnome.nix
+++ nixos/gnome.nix
@@ -9,7 +9,7 @@ in
       (nerdfonts.override { fonts = [ "FiraCode" ]; })
       fira-code
       firefox
-      gnome.gnome-tweaks
+      gnome-tweaks
       keepassxc
       obsidian
       vscodium
@@ -33,7 +33,7 @@ in
     intel-vaapi-driver = pkgs.intel-vaapi-driver.override { enableHybridCodec = true; };
   };
 
-  hardware.opengl = {
+  hardware.graphics = {
     enable = true;
     extraPackages = with pkgs; [
       intel-media-driver # LIBVA_DRIVER_NAME=iHD
