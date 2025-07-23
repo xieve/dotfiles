462a05b5340ea573001969f0ff216728b97ea7ee
xieve <2876434-xieve@users.noreply.gitlab.com>
2025-07-23 16:41:19 +0200

    feat(thegreatbelow/arm): custom handbrake presets

--- nixos/thegreatbelow/automatic-ripping-machine.nix
+++ nixos/thegreatbelow/automatic-ripping-machine.nix
@@ -8,11 +8,12 @@
 let
   secrets = lib.importTOML ./secrets.toml;
   baseFolder = "/mnt/frail/srv/rips";
-  configFolder = "/etc/arm/config";
+  configFolder = "/etc/arm";
   uid = 950;
   gid = config.users.groups.media.gid;
   uidStr = toString uid;
   gidStr = toString gid;
+  handbrakeArgs = preset: "--preset-import-gui '${configFolder}/handbrake/${preset}.json'";
 in
 {
   services.automatic-ripping-machine = {
@@ -28,6 +29,10 @@ in
       # Media will be put into movies/ and shows/ subdirectories
       COMPLETED_PATH = "/mnt/frail/srv/";
       LOGLEVEL = "DEBUG";
+
+      # HandBrake
+      HB_ARGS_DVD = handbrakeArgs "MKV 720p30 x265 Slow CRF19";
+      HB_ARGS_BD = handbrakeArgs "MKV 1080p30 x265 Slow CRF20";
     };
   };
 
@@ -36,5 +41,11 @@ in
   users.users.arm = {
     group = "media";
     inherit uid;
+  };
+
+  systemd.tmpfiles.settings."50-arm-handbrake-presets" = {
+    "${configFolder}/handbrake"."L+".argument = toString ./handbrake;
+    "${baseFolder}/completed/movies"."L+".argument = "/mnt/frail/srv/movies/";
+    "${baseFolder}/completed/shows"."L+".argument = "/mnt/frail/srv/shows/";
   };
 }
