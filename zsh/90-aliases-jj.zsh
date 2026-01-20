fb02fa60083a308a88fc2331c6ad536213b2dfe0
xieve <2876434-xieve@users.noreply.gitlab.com>
2026-01-20 15:47:43 +0100

    feat(zsh): jj aliases

---
+++ zsh/90-aliases-jj.zsh
@@ -0,0 +1,9 @@
+alias jgf='jj git fetch'
+alias jlg='jj log'
+alias jrb='jj rebase'
+alias jgl="jj git fetch && jj rebase -b @ -d 'trunk()'"
+alias jst='jj status'
+alias jsp='jj split'
+alias jgp='jj git push'
+alias jbs='jj bookmark set'
+
