2946e1438f9936622b8f2c03e6402a300f8c456c
xieve <2876434-xieve@users.noreply.gitlab.com>
2026-04-04 19:56:25 +0200

    fix(warmplace): always repair fs on boot, even if unsafe
    enabled systemd in initrd since the nixos stage 1 bash script can't
    force unsafe fsck repairs

--- nixos/warmplace/configuration.nix
+++ nixos/warmplace/configuration.nix
@@ -32,12 +32,14 @@ in
   };
 
   boot = {
+    initrd.systemd.enable = true;
     kernelParams = [
       "console=ttyS0,115200n8"
       "console=tty0"
       ''root="LABEL=nixos"''
       "fsck.repair=yes" # Always try to repair a broken fs, even if deemed risky
       "fsck.mode=force" # Always check filesystem, even if deemed unnecessary
+      "ro" # Necessary for systemd-fsck, will be remounted rw afterwards
     ];
     # Override common.nix
     loader.systemd-boot.enable = false;
